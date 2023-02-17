5c2a8e6f3f6c494d4cfb800ad69099ec4f89c654
Raudyah Maghfirah <12050120457@students.uin-suska.ac.id>
2023-02-17 21:14:17 +0700

    asset, color, font

--- lib/ui/theme/colors.dart
+++ lib/ui/theme/colors.dart
@@ -3,10 +3,15 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color teaGreen = Color(0xFFD6FFB7);
-  static const Color canary = Color(0xFFF5FF90);
-  static const Color maximumYellowRed = Color(0xFFFFC15E);
-  static const Color orangePeel = Color(0xFFFF9F1C);
-  static const Color midnightBlue = Color(0xFF080357);
-  static const Color black = Colors.black;
+  static const Color primary = Color(0xFFFFFFFF);
+  static const Color green = Color(0x92EE87);
+  static const Color green30 = Color(0x28AC3D);
+  static const Color green50 = Color(0xC3F1BD);
+  static const Color black = Color(0x4A4646);
+  static const Color red = Color(0xDD3434);
+  static const Color pink = Color(0xE89494);
+  static const Color blue = Color(0xAFE3E6);
+  static const Color grey = Color(0xD9D9D9);
+  
+
 }
